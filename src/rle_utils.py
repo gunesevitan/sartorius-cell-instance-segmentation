77fed51a50767d7d2a7333d2a6a5780c450e2b79
gunesevitan <gunesevitan@gmail.com>
2021-10-16 23:10:02 +0300

    rle decode

---
+++ src/rle_utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+def decode_rle_mask(rle_mask, shape):
+
+    """
+    Decode run-length encoded segmentation mask string into 2d array
+
+    Parameters
+    ----------
+    rle_mask (str): Run-length encoded segmentation mask string
+    shape (tuple): Height and width of the mask
+
+    Returns
+    -------
+    mask [numpy.ndarray of shape (height, width)]: Decoded 2d segmentation mask
+    """
+
+    rle_mask = rle_mask.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (rle_mask[0:][::2], rle_mask[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+
+    mask = np.zeros((shape[0] * shape[1]), dtype=np.uint8)
+    for start, end in zip(starts, ends):
+        mask[start:end] = 1
+
+    return mask.reshape(shape[0], shape[1])
+
+
+def get_mask(df, image_id, shape):
+
+    """
+    Decode all run-length encoded segmentation masks of a given image and merge them into a 2d array
+
+    Parameters
+    ----------
+    df [pandas.DataFrame of shape (n_annotation, >= 2)]: Dataframe with id and annotation columns
+    image_id (str): Image ID (filename)
+    shape (tuple): Height and width of the mask
+
+    Returns
+    -------
+    mask [numpy.ndarray of shape (height, width)]: Decoded 2d segmentation mask
+    """
+
+    mask = np.zeros((shape[0], shape[1]), dtype=np.uint8)
+    rle_masks = df.loc[df['id'] == image_id, 'annotation'].values
+    for rle_mask in rle_masks:
+        mask += decode_rle_mask(rle_mask, shape)
+
+    return mask
