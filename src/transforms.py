436886de1ddcf3b0c0ac0de69ec58162a9af843f
gunesevitan <gunesevitan@gmail.com>
2021-10-17 15:51:46 +0300

    added totensorv2

--- src/transforms.py
+++ src/transforms.py
@@ -1,4 +1,5 @@
 import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 
 
 def get_transforms(**transform_args):
@@ -7,10 +8,12 @@ def get_transforms(**transform_args):
         A.Resize(*transform_args['resize_shape'], interpolation=4, always_apply=True, p=1),
         A.HorizontalFlip(p=transform_args['horizontal_flip_probability']),
         A.VerticalFlip(p=transform_args['vertical_flip_probability']),
+        ToTensorV2()
     ])
 
     test_transforms = A.Compose([
         A.Resize(*transform_args['resize_shape'], interpolation=4, always_apply=True, p=1),
+        ToTensorV2()
     ])
 
     return {'train': train_transforms, 'test': test_transforms}
