ecd8ffbd4cc8ac3c0ad26802af0a77a164caba30
gunesevitan <gunesevitan@gmail.com>
2021-10-22 15:09:16 +0300

    instance segmentation dataset

--- src/datasets.py
+++ src/datasets.py
@@ -4,9 +4,10 @@ import torch
 from torch.utils.data import Dataset
 
 import settings
+import mask_utils
 
 
-class SegmentationDataset(Dataset):
+class InstanceSegmentationDataset(Dataset):
 
     def __init__(self, images, masks=None, transforms=None):
 
@@ -28,16 +29,108 @@ class SegmentationDataset(Dataset):
 
         Returns
         -------
-        mri [torch.FloatTensor of shape (channel, depth, height, width)]: 4D mpMRI
-        mask [torch.FloatTensor of shape (channel, depth, height, width)]: 4D segmentation mask
+        image [torch.FloatTensor of shape (depth, height, width)]: Image
+        target (dict or None):
+        Dictionary contains:
+            - masks [torch.FloatTensor of shape (n_objects, height, width)]: Segmentation masks
+            - boxes [torch.FloatTensor of shape (n_objects, 4)]: Bounding boxes in VOC format
+            - labels [torch.Int64Tensor of shape (n_objects)]: Labels of objects
+            - image_id [torch.Int64Tensor of shape (1)]: Image identifier used during evaluation
+            - area [torch.FloatTensor of shape (n_objects)]: Areas of the bounding boxes
+            - iscrowd [torch.UInt8Tensor of shape (n_objects)]: Instances with iscrowd=True will be ignored during evaluation
         """
 
         image = cv2.imread(f'{settings.DATA_PATH}/train_images/{self.images[idx]}.png')
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        if self.masks == 'npy':
+        if self.masks is not None:
 
-            mask = np.load(f'{settings.DATA_PATH}/train_masks/{self.images[idx]}.npy')
+            masks = []
+            boxes = []
+            labels = []
+
+            for mask in self.masks[idx]:
+                decoded_mask = mask_utils.decode_rle_mask(rle_mask=mask, shape=image.shape)
+                bounding_box = mask_utils.get_bounding_box(decoded_mask)
+                masks.append(decoded_mask)
+                boxes.append(bounding_box)
+                labels.append(1)
+
+            if self.transforms is not None:
+                transformed = self.transforms(image=image, masks=masks, bboxes=boxes, labels=labels)
+                image = transformed['image']
+                masks = transformed['masks']
+                boxes = transformed['bboxes']
+                labels = transformed['labels']
+
+            image_id = torch.tensor([idx])
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+            masks = torch.as_tensor(masks, dtype=torch.uint8)
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            iscrowd = torch.zeros((len(masks),), dtype=torch.int64)
+
+            target = {
+                'masks': masks,
+                'boxes': boxes,
+                'labels': labels,
+                'image_id': image_id,
+                'area': area,
+                'iscrowd': iscrowd
+            }
+
+            return image, target
+
+        else:
+
+            if self.transforms is not None:
+                transformed = self.transforms(image=image)
+                image = transformed['image']
+            else:
+                image = np.stack([image] * 3)
+                image = torch.as_tensor(image, dtype=torch.float32)
+
+            return image
+
+
+class SemanticSegmentationDataset(Dataset):
+
+    def __init__(self, images, masks=None, transforms=None):
+
+        self.images = images
+        self.masks = masks
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        """
+        Get the idxth element in the dataset
+
+        Parameters
+        ----------
+        idx (int): Index of the sample (0 <= idx < len(self.images))
+
+        Returns
+        -------
+        image [torch.FloatTensor of shape (depth, height, width)]: Image
+        mask [torch.FloatTensor of shape (depth, height, width)]: Semantic segmentation mask
+        """
+
+        image = cv2.imread(f'{settings.DATA_PATH}/train_images/{self.images[idx]}.png')
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        if self.masks is not None:
+
+            masks = []
+
+            for mask in self.masks[idx]:
+                decoded_mask = mask_utils.decode_rle_mask(rle_mask=mask, shape=image.shape)
+                masks.append(decoded_mask)
+
+            mask = np.any(np.stack(masks), axis=0)
 
             if self.transforms is not None:
                 transformed = self.transforms(image=image, mask=mask)
@@ -51,7 +144,7 @@ class SegmentationDataset(Dataset):
 
             return image, mask
 
-        elif self.masks is None:
+        else:
 
             if self.transforms is not None:
                 transformed = self.transforms(image=image)
