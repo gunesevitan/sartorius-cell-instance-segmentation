db672e2e352534a94f51d8be87ca4455e2778691
gunesevitan <gunesevitan@gmail.com>
2021-10-23 08:23:21 +0300

    path fix

--- src/transforms.py
+++ src/transforms.py
@@ -48,7 +48,6 @@ def get_transforms(**kwargs):
     bbox_params = {'format': 'pascal_voc',  'label_fields': ['labels'], 'min_area': 0, 'min_visibility': 0}
 
     train_transforms = A.Compose([
-        #A.Resize(height=kwargs['resize']['height'], width=kwargs['resize']['width'], interpolation=1, always_apply=True),
         A.HorizontalFlip(p=kwargs['horizontal_flip_probability']),
         A.VerticalFlip(p=kwargs['vertical_flip_probability']),
         Scale(always_apply=True),
@@ -57,10 +56,9 @@ def get_transforms(**kwargs):
     ], bbox_params=A.BboxParams(**bbox_params))
 
     test_transforms = A.Compose([
-        #A.Resize(height=kwargs['resize']['height'], width=kwargs['resize']['width'], interpolation=1, always_apply=True, p=1),
         Scale(always_apply=True),
         ToRGB(always_apply=True),
         ToTensorV2(always_apply=True)
-    ])
+    ], bbox_params=A.BboxParams(**bbox_params))
 
     return {'train': train_transforms, 'test': test_transforms}
