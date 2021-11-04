3b10fe02f912fbec9a8361e90817318adbce7fb8
gunesevitan <gunesevitan@gmail.com>
2021-11-04 09:56:30 +0300

    classification trainer

--- src/trainers.py
+++ src/trainers.py
@@ -1,15 +1,18 @@
 from tqdm import tqdm
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 import torch.optim as optim
+from sklearn.metrics import accuracy_score
 
 import settings
 import training_utils
 import inference_utils
 import visualization
 import transforms
-from datasets import InstanceSegmentationDataset
+import datasets
 import pytorch_models
 
 
@@ -136,14 +139,14 @@ class InstanceSegmentationTrainer:
     def train_and_validate(self, df):
 
         print(f'\n{"-" * 30}\nRunning {self.model} for Training\n{"-" * 30}\n')
-        instance_segmentation_transforms = transforms.get_transforms(**self.transform_parameters)
+        instance_segmentation_transforms = transforms.get_instance_segmentation_transforms(**self.transform_parameters)
 
         for fold in sorted(df['fold'].unique()):
 
             print(f'\nFold {fold}\n{"-" * 6}')
 
             trn_idx, val_idx = df.loc[df['fold'] != fold].index, df.loc[df['fold'] == fold].index
-            train_dataset = InstanceSegmentationDataset(
+            train_dataset = datasets.InstanceSegmentationDataset(
                 images=df.loc[trn_idx, 'id'].values,
                 masks=df.loc[trn_idx, 'annotation'].values,
                 transforms=instance_segmentation_transforms['train']
@@ -157,7 +160,7 @@ class InstanceSegmentationTrainer:
                 collate_fn=training_utils.collate_fn,
                 num_workers=self.training_parameters['data_loader']['num_workers'],
             )
-            val_dataset = InstanceSegmentationDataset(
+            val_dataset = datasets.InstanceSegmentationDataset(
                 images=df.loc[val_idx, 'id'].values,
                 masks=df.loc[val_idx, 'annotation'].values,
                 transforms=instance_segmentation_transforms['test']
@@ -224,7 +227,7 @@ class InstanceSegmentationTrainer:
     def inference(self, df):
 
         print(f'\n{"-" * 30}\nRunning {self.model} for Training\n{"-" * 30}\n')
-        instance_segmentation_transforms = transforms.get_transforms(**self.transform_parameters)
+        instance_segmentation_transforms = transforms.get_instance_segmentation_transforms(**self.transform_parameters)
         df[f'{self.model_path}_predictions_average_precision'] = 0
 
         for fold in sorted(df['fold'].unique()):
@@ -232,7 +235,7 @@ class InstanceSegmentationTrainer:
             print(f'\nFold {fold}\n{"-" * 6}')
 
             _, val_idx = df.loc[df['fold'] != fold].index, df.loc[df['fold'] == fold].index
-            val_dataset = InstanceSegmentationDataset(
+            val_dataset = datasets.InstanceSegmentationDataset(
                 images=df.loc[val_idx, 'id'].values,
                 masks=df.loc[val_idx, 'annotation'].values,
                 transforms=instance_segmentation_transforms['val']
@@ -274,3 +277,189 @@ class InstanceSegmentationTrainer:
             print(f'Fold {fold} - mAP: {fold_score:.6f}')
         oof_score = np.mean(df[f'{self.model_path}_predictions_average_precision'])
         print(f'{"-" * 30}\nOOF mAP: {oof_score:.6}\n{"-" * 30}')
+
+
+class ClassificationTrainer:
+
+    def __init__(self, model, model_path, model_parameters, training_parameters, transform_parameters):
+
+        self.model = model
+        self.model_path = model_path
+        self.model_parameters = model_parameters
+        self.training_parameters = training_parameters
+        self.transform_parameters = transform_parameters
+
+    def train_fn(self, train_loader, model, criterion, optimizer, scheduler, device):
+
+        """
+        Train given model on given data loader
+
+        Parameters
+        ----------
+        train_loader (torch.utils.data.DataLoader): Training set data loader
+        model (torch.nn.Module): Model to train
+        criterion (torch.nn.modules.loss): Loss function
+        optimizer (torch.optim.Optimizer): Optimizer
+        device (torch.device): Location of the model and inputs
+        scheduler (torch.optim.LRScheduler or None): Learning rate scheduler
+
+        Returns
+        -------
+        train_loss (float): Average training loss after model is fully trained on training set data loader
+        """
+
+        print('\n')
+        model.train()
+        progress_bar = tqdm(train_loader)
+        losses = []
+
+        for images, targets in progress_bar:
+            images, targets = images.to(device), targets.to(device)
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, targets)
+            loss.backward()
+            optimizer.step()
+            if scheduler is not None:
+                scheduler.step()
+            losses.append(loss.item())
+            average_loss = np.mean(losses)
+            lr = scheduler.get_last_lr()[0] if scheduler is not None else optimizer.param_groups[0]['lr']
+            progress_bar.set_description(f'train_loss: {average_loss:.6f} - lr: {lr:.8f}')
+
+        train_loss = np.mean(losses)
+        return train_loss
+
+    def val_fn(self, val_loader, model, criterion, device):
+
+        """
+        Validate given model on given data loader
+
+        Parameters
+        ----------
+        val_loader (torch.utils.data.DataLoader): Validation set data loader
+        model (torch.nn.Module): Model to validate
+        criterion (torch.nn.modules.loss): Loss function
+        device (torch.device): Location of the model and inputs
+
+        Returns
+        -------
+        val_accuracy (float): Validation accuracy
+        val_loss (float): Average validation loss after model is fully validated on validation set data loader
+        """
+
+        model.eval()
+        progress_bar = tqdm(val_loader)
+        losses = []
+        labels = []
+        predictions = []
+
+        with torch.no_grad():
+            for images, targets in progress_bar:
+                images, targets = images.to(device), targets.to(device)
+                output = model(images)
+                loss = criterion(output, targets)
+                losses.append(loss.item())
+                average_loss = np.mean(losses)
+                progress_bar.set_description(f'val_loss: {average_loss:.6f}')
+                labels += targets.detach().cpu().numpy().tolist()
+                predictions += F.softmax(output.detach().cpu(), dim=1).numpy().tolist()
+
+        predictions = np.array(predictions)
+        labels = np.array(labels)
+        predictions = np.argmax(predictions, axis=1)
+        val_accuracy = accuracy_score(y_true=labels, y_pred=predictions)
+        val_loss = np.mean(losses)
+        return val_accuracy, val_loss
+
+    def train_and_validate(self, df):
+
+        """
+        Train on train_images and validate on train_semi_supervised_images with specified configurations
+
+        Parameters
+        ----------
+        df [pandas.DataFrame of shape (2578, 3)]: Dataframe of filenames and targets
+        """
+
+        print(f'\n{"-" * 30}\nRunning {self.model} for Training\n{"-" * 30}\n')
+
+        classification_transforms = transforms.get_classification_transforms(**self.transform_parameters)
+        trn_idx, val_idx = df.loc[df['fold'] == 'train'].index, df.loc[df['fold'] == 'val'].index
+        train_dataset = datasets.ClassificationDataset(
+            images=df.loc[trn_idx, 'id'].values,
+            targets=df.loc[trn_idx, 'cell_type_target'].values,
+            image_directory='train_images',
+            transforms=classification_transforms['train'],
+        )
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=self.training_parameters['data_loader']['training_batch_size'],
+            sampler=RandomSampler(train_dataset),
+            pin_memory=True,
+            drop_last=False,
+            num_workers=self.training_parameters['data_loader']['num_workers']
+        )
+        val_dataset = datasets.ClassificationDataset(
+            images=df.loc[val_idx, 'id'].values,
+            targets=df.loc[val_idx, 'cell_type_target'].values,
+            image_directory='train_semi_supervised_images',
+            transforms=classification_transforms['val'],
+        )
+        val_loader = DataLoader(
+            val_dataset,
+            batch_size=self.training_parameters['data_loader']['val_batch_size'],
+            sampler=SequentialSampler(val_dataset),
+            pin_memory=True,
+            drop_last=False,
+            num_workers=self.training_parameters['data_loader']['num_workers']
+        )
+
+        training_utils.set_seed(self.training_parameters['random_state'], deterministic_cudnn=self.training_parameters['deterministic_cudnn'])
+        device = torch.device(self.training_parameters['device'])
+        criterion = nn.CrossEntropyLoss()
+        model = getattr(pytorch_models, self.model)(**self.model_parameters)
+        model = model.to(device)
+        optimizer = getattr(optim, self.training_parameters['optimizer'])(model.parameters(), **self.training_parameters['optimizer_parameters'])
+        scheduler = getattr(optim.lr_scheduler, self.training_parameters['lr_scheduler'])(optimizer, **self.training_parameters['lr_scheduler_parameters'])
+
+        early_stopping = False
+        summary = {
+            'train_loss': [],
+            'val_loss': []
+        }
+
+        for epoch in range(1, self.training_parameters['epochs'] + 1):
+
+            if early_stopping:
+                break
+
+            if self.training_parameters['lr_scheduler'] == 'ReduceLROnPlateau':
+                train_loss = self.train_fn(train_loader=train_loader, model=model, criterion=criterion, optimizer=optimizer, scheduler=None, device=device)
+                val_accuracy, val_loss = self.val_fn(val_loader=val_loader, model=model, criterion=criterion, device=device)
+                scheduler.step(val_loss)
+            else:
+                train_loss = self.train_fn(train_loader=train_loader, model=model, criterion=criterion, optimizer=optimizer, scheduler=scheduler, device=device)
+                val_accuracy, val_loss = self.val_fn(val_loader=val_loader, model=model, criterion=criterion, device=device)
+
+            print(f'Epoch {epoch} - Training Loss: {train_loss:.6f} - Validation Loss: {val_loss:.6f} - Validation Accuracy: {val_accuracy:6f}')
+            best_val_loss = np.min(summary['val_loss']) if len(summary['val_loss']) > 0 else np.inf
+            if val_loss < best_val_loss:
+                model_path = f'{settings.MODELS_PATH}/{self.model_path}/{self.model_path}.pt'
+                torch.save(model.state_dict(), model_path)
+                print(f'Saving model to {model_path} (validation loss decreased from {best_val_loss:.6f} to {val_loss:.6f})')
+
+            summary['train_loss'].append(train_loss)
+            summary['val_loss'].append(val_loss)
+
+            best_iteration = np.argmin(summary['val_loss']) + 1
+            if len(summary['val_loss']) - best_iteration >= self.training_parameters['early_stopping_patience']:
+                print(f'Early stopping (validation loss didn\'t increase for {self.training_parameters["early_stopping_patience"]} epochs/steps)')
+                print(f'Best validation loss is {np.min(summary["val_loss"]):.6f}')
+                visualization.visualize_learning_curve(
+                    training_losses=summary['train_loss'],
+                    validation_losses=summary['val_loss'],
+                    title=f'{self.model_path} - Learning Curve',
+                    path=f'{settings.MODELS_PATH}/{self.model_path}/{self.model_path}_learning_curve.png'
+                )
+                early_stopping = True
