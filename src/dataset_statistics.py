ebd824b389e752674f569d22a38f0df3a8c85816
gunesevitan <gunesevitan@gmail.com>
2021-11-04 09:35:40 +0300

    scaled and non-scaled dataset statistics

--- src/dataset_statistics.py
+++ src/dataset_statistics.py
@@ -6,10 +6,12 @@ import cv2
 import settings
 
 
-# Mean and standard deviations below are computed with the code below
 TRAIN_STATISTICS = {'mean': 127.96482685978934, 'std': 13.705081432885466}
 TRAIN_TEST_STATISTICS = {'mean': 127.96497968626201, 'std': 13.686623357801329}
 TRAIN_TEST_SEMI_SUPERVISED_STATISTICS = {'mean': 127.9755108670509, 'std': 12.206579996359626}
+TRAIN_STATISTICS_SCALED = {'mean': 0.5018228504305459, 'std': 0.05374541738386454}
+TRAIN_TEST_STATISTICS_SCALED = {'mean': 0.5018234497500464, 'std': 0.05367303277569167}
+TRAIN_TEST_SEMI_SUPERVISED_STATISTICS_SCALED = {'mean': 0.5018395185470581, 'std': 0.04786895215511322}
 
 
 if __name__ == '__main__':
@@ -24,7 +26,7 @@ if __name__ == '__main__':
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         images.append(image)
 
-    images = np.stack(images)
+    images = np.float32(np.stack(images)) / 255.
     image_mean = np.mean(images)
     image_std = np.std(images)
     print(f'{images.shape[0]} Images - Mean: {image_mean} - Standard Deviation: {image_std}')
