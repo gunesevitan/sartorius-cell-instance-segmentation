a3ee531df00e21c0d3296ee84cbe81979b49fda8
gunesevitan <gunesevitan@gmail.com>
2021-11-13 19:29:45 +0300

    det

--- src/detectron_trainer.py
+++ src/detectron_trainer.py
@@ -8,11 +8,39 @@ from detectron2.evaluation.evaluator import DatasetEvaluator
 from detectron2.engine import DefaultPredictor, DefaultTrainer
 
 import settings
+import metrics
 
 
+class IoUmAPEvaluator(DatasetEvaluator):
+
+    def __init__(self, dataset_name):
+
+        dataset = DatasetCatalog.get(dataset_name)
+        self.annotations_cache = {item['image_id']: item['annotations'] for item in dataset}
+        self.scores = []
+
+    def reset(self):
+        self.scores = []
+
+    def process(self, inputs, outputs):
+
+        print(inputs)
+        print(outputs)
+
+        for input_, output in zip(inputs, outputs):
+            if len(output['instances']) == 0:
+                self.scores.append(0)
+            else:
+                ground_truth_masks = self.annotations_cache[input_['image_id']]
+                #score = get_average_precision_detectron()
+                #self.scores.append(score(out, targ))
 
 
+class InstanceSegmentationTrainer(DefaultTrainer):
 
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        return IoUmAPEvaluator(dataset_name)
 
 
 
@@ -54,10 +82,10 @@ class DetectronTrainer:
             cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
             cfg.DATASETS.TRAIN = (f'training_set_fold{fold}',)
             cfg.DATASETS.TEST = (f'validation_set_fold{fold}',)
-            cfg.DATALOADER.NUM_WORKERS = 2
+            cfg.DATALOADER.NUM_WORKERS = 0
             cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-            cfg.SOLVER.IMS_PER_BATCH = 4
-            cfg.SOLVER.BASE_LR = 0.0005
+            cfg.SOLVER.IMS_PER_BATCH = 1
+            cfg.SOLVER.BASE_LR = 0.002
             cfg.SOLVER.MAX_ITER = 1000
             cfg.SOLVER.STEPS = []
             cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
@@ -65,7 +93,7 @@ class DetectronTrainer:
             cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
             cfg.TEST.EVAL_PERIOD = len(DatasetCatalog.get(f'training_set_fold{fold}')) // cfg.SOLVER.IMS_PER_BATCH
 
-            trainer = DefaultTrainer(cfg)
+            trainer = InstanceSegmentationTrainer(cfg)
             trainer.resume_or_load(resume=False)
             trainer.train()
 
@@ -73,7 +101,7 @@ class DetectronTrainer:
 if __name__ == '__main__':
 
     trainer = DetectronTrainer(model='Detectron')
-    m, d, c = trainer.train_and_validate()
+    trainer.train_and_validate()
 
     import cv2
     import matplotlib.pyplot as plt
