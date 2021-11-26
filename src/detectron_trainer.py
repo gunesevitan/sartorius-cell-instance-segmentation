24433415585a29ce36bce00bd75d4afc8ee18f21
gunesevitan <gunesevitan@gmail.com>
2021-11-26 18:43:51 +0300

    trainer

--- src/detectron_trainer.py
+++ src/detectron_trainer.py
@@ -1,6 +1,6 @@
 import yaml
 import argparse
-from detectron2.config import get_cfg, CfgNode
+from detectron2.config import get_cfg
 from detectron2.data import DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
 from detectron2 import model_zoo
@@ -24,18 +24,18 @@ if __name__ == '__main__':
         json_file=f'{settings.DATA_PATH}/coco_datasets/{trainer_config["DATASET"]["training_set_name"]}.json',
         image_root=trainer_config['DATASET']['image_root']
     )
-    register_coco_instances(
-        name=trainer_config['DATASET']['test_set_name'],
-        metadata={},
-        json_file=f'{settings.DATA_PATH}/coco_datasets/{trainer_config["DATASET"]["test_set_name"]}.json',
-        image_root=trainer_config['DATASET']['image_root']
-    )
+    if trainer_config['DATASET']['test_set_name'] is not None:
+        register_coco_instances(
+            name=trainer_config['DATASET']['test_set_name'],
+            metadata={},
+            json_file=f'{settings.DATA_PATH}/coco_datasets/{trainer_config["DATASET"]["test_set_name"]}.json',
+            image_root=trainer_config['DATASET']['image_root']
+        )
     train_dataset = DatasetCatalog.get(trainer_config['DATASET']['training_set_name'])
 
     detectron_config.merge_from_file(model_zoo.get_config_file(trainer_config['MODEL']['path']))
     detectron_config.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(trainer_config['MODEL']['path'])
     detectron_config.merge_from_file(f'{args.config_path}/detectron_config.yaml')
-    detectron_config['AUGMENTATIONS'] = CfgNode(trainer_config['AUGMENTATIONS'])
 
     trainer = detectron_utils.InstanceSegmentationTrainer(detectron_config)
     trainer.resume_or_load(resume=False)
