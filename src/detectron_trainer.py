10f8dec0afd92893f0245323330bbd3a5a63148b
gunesevitan <gunesevitan@gmail.com>
2021-11-28 10:39:11 +0300

    merge

--- src/detectron_trainer.py
+++ src/detectron_trainer.py
@@ -38,5 +38,5 @@ if __name__ == '__main__':
     detectron_config.merge_from_file(f'{args.config_path}/detectron_config.yaml')
 
     trainer = detectron_utils.InstanceSegmentationTrainer(detectron_config)
-    trainer.resume_or_load(resume=False)
+    trainer.resume_or_load(resume=True)
     trainer.train()
