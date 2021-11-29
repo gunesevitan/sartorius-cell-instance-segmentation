6eba79e0933d52940ef942f21870a83efa36eae2
gunesevitan <gunesevitan@gmail.com>
2021-11-29 17:53:10 +0300

    merge

--- src/detectron_trainer.py
+++ src/detectron_trainer.py
@@ -49,4 +49,4 @@ if __name__ == '__main__':
     elif args.mode == 'eval':
         detectron_config.MODEL.WEIGHTS = '../models/competition/detectron_mask_rcnn_clean_fold3.pth'
         model = trainer.build_model(detectron_config)
-        trainer.test(cfg=detectron_config, model=model)
+        res = trainer.test(cfg=detectron_config, model=model)
