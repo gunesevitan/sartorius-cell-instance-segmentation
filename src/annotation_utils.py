c6cca3728af07afc1e2efa7ff34d4a0f191b3a36
gunesevitan <gunesevitan@gmail.com>
2021-12-01 15:12:20 +0300

    merge

--- src/annotation_utils.py
+++ src/annotation_utils.py
@@ -203,8 +203,11 @@ def polygon_to_mask(polygon, shape):
     """
 
     # Convert numpy.array to list of tuple pairs of X and Y coordinates
+    print(polygon)
     points = np.array(polygon).reshape(-1, 2)
+    print(points)
     points = [(point[0], point[1]) for point in points]
+    print(points)
     mask = Image.new('L', (shape[1], shape[0]), 0)
 
     # Draw mask from the polygon
