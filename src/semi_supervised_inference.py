fc6705e5162fb43a53b014805e8ea877ef7148fe
gunesevitan <gunesevitan@gmail.com>
2021-12-15 17:17:00 +0300

    semi

--- src/semi_supervised_inference.py
+++ src/semi_supervised_inference.py
@@ -14,7 +14,7 @@ import post_processing
 if __name__ == '__main__':
 
     df = pd.read_csv(f'{settings.DATA_PATH}/train_processed.csv')
-    semi_supervised_images = df.loc[df['annotation'].isnull(), 'id'].values
+    df_semi_supervised = df.loc[df['annotation'].isnull(), :].reset_index(drop=True)
     detectron2_mask_rcnn_models = detectron_inference.load_detectron2_models(
         model_directory=f'{settings.MODELS_PATH}/competition/detectron2_mask_rcnn2',
         folds_to_use=[1]
@@ -37,9 +37,15 @@ if __name__ == '__main__':
         }
     }
 
-    df_semi_supervised = pd.DataFrame(columns=['id', 'annotation', 'annotation_filled'])
+    df_semi_supervised_annotations = pd.DataFrame(columns=df_semi_supervised.columns)
 
-    for image_id in tqdm(semi_supervised_images):
+    for idx in tqdm(df_semi_supervised.index):
+
+        image_id = df_semi_supervised.loc[idx, 'id']
+        cell_type_label = df_semi_supervised.loc[idx, 'cell_type']
+        plate_time = df_semi_supervised.loc[idx, 'plate_time']
+        sample_date = df_semi_supervised.loc[idx, 'sample_date']
+        sample_id = df_semi_supervised.loc[idx, 'sample_id']
 
         all_prediction_boxes = []
         all_prediction_masks = []
@@ -65,21 +71,37 @@ if __name__ == '__main__':
             cell_types.append(cell_type)
         cell_type = mode(cell_types)[0][0]
 
-        processed_masks = post_processing.blend_masks(
+        blended_masks = post_processing.blend_masks(
             prediction_boxes=all_prediction_boxes,
             prediction_masks=all_prediction_masks,
             iou_threshold=0.9,
             label_threshold=0.5,
             drop_single_components=True
         )
-        processed_masks = post_processing.fix_overlaps(
-            processed_masks,
+        blended_masks = post_processing.fix_overlaps(
+            blended_masks,
             area_threshold=detectron2_mask_rcnn_post_processing_parameters['area_thresholds'][cell_type],
             mask_area_order='descending'
         )
 
-        for mask in processed_masks:
+        for mask in blended_masks:
             rle_encoded_mask = annotation_utils.encode_rle_mask(mask)
-            df_semi_supervised = df_semi_supervised.append({'id': image_id, 'annotation': rle_encoded_mask, 'annotation_filled': rle_encoded_mask}, ignore_index=True)
-            
+            df_semi_supervised_annotations = df_semi_supervised_annotations.append({
+                'id': image_id,
+                'annotation': rle_encoded_mask,
+                'width': 704,
+                'height': 520,
+                'cell_type': cell_type_label,
+                'plate_time': plate_time,
+                'sample_date': sample_date,
+                'sample_id': sample_id,
+                'stratified_fold': -1,
+                'non_noisy_split': -1,
+                'annotation_filled': rle_encoded_mask,
+                'annotation_broken': False
+            }, ignore_index=True)
+
         break
+
+    df_labeled = df.loc[~df['annotation'].isnull(), :].reset_index(drop=True)
+    df_labeled = pd.concat([df_labeled, df_semi_supervised_annotations], axis=0, ignore_index=True)
