a1a61919f9b2d83562c05e433007f9918de3366b
gunesevitan <gunesevitan@gmail.com>
2021-12-16 10:31:21 +0300

    semi

---
+++ src/semi_supervised_preprocessing.py
@@ -0,0 +1,26 @@
+from tqdm import tqdm
+from glob import glob
+import numpy as np
+import cv2
+
+import settings
+
+if __name__ == '__main__':
+
+    train_image_paths = glob(f'{settings.DATA_PATH}/train_images/*.png')
+    train_semi_supervised_image_paths = glob(f'{settings.DATA_PATH}/train_semi_supervised_images/*.png')
+
+    train_images = []
+    for train_image_path in tqdm(train_image_paths):
+        image = cv2.imread(train_image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        train_images.append(image)
+
+    train_semi_supervised_images = []
+    for train_semi_supervised_image_path in tqdm(train_semi_supervised_image_paths):
+        image = cv2.imread(train_semi_supervised_image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        train_semi_supervised_images.append(image)
+
+    x = np.isin(train_semi_supervised_images, train_images)
+    
